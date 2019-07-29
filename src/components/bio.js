8ac58be93290b7ce015d740d0c3a774e3c4c6aea
Ryan O'Neill <ryanoneill@gmail.com>
2019-07-28 19:27:29 -0700

    Updated to me

--- src/components/bio.js
+++ src/components/bio.js
@@ -54,12 +54,9 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
+        Written by <a href={`https://twitter.com/${social.twitter}`}><strong>{author}</strong></a> who
+        lives in San Francisco and works at Twitter building Finagle.
         {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
-        </a>
       </p>
     </div>
   )
